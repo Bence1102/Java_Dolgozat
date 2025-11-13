da3a9c0a1ee22811013473b0b69863f9a7228137
Bence1102 <szente.bence04@gmail.com>
2025-11-13 12:55:10 +0100

    Modell kész

--- src/modell/LightOnJatekModell.java
+++ src/modell/LightOnJatekModell.java
@@ -3,5 +3,43 @@ package modell;
 
 
 public class LightOnJatekModell {
+    private boolean[][] jatekter;
+    public LightOnJatekModell(){
+        jatekter= new boolean[3][3];
+    }
     
+    private void kapcsolo(int sorok, int oszlopok){
+        csere(sorok,oszlopok);
+        if (sorok > 0) csere (sorok -1,oszlopok);
+        if (sorok < 2) csere (sorok+1,oszlopok);
+        if (oszlopok > 0) csere (sorok, oszlopok -1);
+        if (oszlopok < 2) csere (sorok, oszlopok +1);
+        
+    }
+    
+    private void csere(int sorok,int oszlopok){
+        jatekter[sorok][oszlopok] =! jatekter[sorok][oszlopok];
+    }
+    
+    private boolean getErtek(int sorok,int oszlopok){
+        
+        return jatekter[sorok][oszlopok];   
+    }
+    
+    public void mehet(){
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                jatekter[i][j] = false;
+            }
+        }
+    }
+    
+    public boolean nyertes(){
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if(!jatekter[i][j]) return false;
+            }
+        }
+        return true;
+    }
 }

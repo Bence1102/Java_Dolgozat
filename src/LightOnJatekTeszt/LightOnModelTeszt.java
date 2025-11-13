c03359d929a88cd077cb9444ac61ad258c29ca0a
Bence1102 <szente.bence04@gmail.com>
2025-11-13 15:37:11 +0100

    Model teszt kesz

--- src/LightOnJatekTeszt/LightOnModelTeszt.java
+++ src/LightOnJatekTeszt/LightOnModelTeszt.java
@@ -1,13 +1,75 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package LightOnJatekTeszt;
 
-/**
- *
- * @author szente.bence
- */
+import modell.LightOnJatekModell;
+
 public class LightOnModelTeszt {
+
+    private LightOnJatekModell model;
+
     
+    public void setUp() {
+        model = new LightOnJatekModell();
+    }
+
+    
+    public void testMehetAlaphelyzet() {
+        model.mehet();
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                assertFalse(model.getErtek(i, j), "Minden mezőnek false-nak kell lennie");
+            }
+        }
+    }
+
+    
+    public void testKapcsoloMegvaltoztatjaErteket() {
+        model.mehet();
+        model.kapcsolo(1, 1);
+        assertTrue(model.getErtek(1, 1), "Középső mezőnek true-nak kell lennie");
+        assertTrue(model.getErtek(0, 1), "Felül szomszéd mező true");
+        assertTrue(model.getErtek(2, 1), "Alul szomszéd mező true");
+        assertTrue(model.getErtek(1, 0), "Bal szomszéd mező true");
+        assertTrue(model.getErtek(1, 2), "Jobb szomszéd mező true");
+    }
+
+    
+    public void testNyertesFalse() {
+        model.mehet();
+        assertFalse(model.nyertes(), "Üres mezőkkel nem lehet nyertes");
+    }
+
+    
+    public void testNyertesTrue() {
+        for (int i = 0; i < 3; i++)
+            for (int j = 0; j < 3; j++)
+                model.kapcsolo(i, j); 
+        boolean allTrue = true;
+        for (int i = 0; i < 3; i++)
+            for (int j = 0; j < 3; j++)
+                allTrue &= model.getErtek(i, j);
+        if(allTrue)
+            assertTrue(model.nyertes(), "Minden mező true, nyertesnek kell lennie");
+    }
+
+    
+    public void testUjgomb() {
+        model.ujgomb();
+        int countTrue = 0;
+        for (int i = 0; i < 3; i++)
+            for (int j = 0; j < 3; j++)
+                if(model.getErtek(i,j)) countTrue++;
+        assertEquals(1, countTrue, "Csak 1 mező legyen true az ujgomb után");
+    }
+
+    private void assertFalse(boolean ertek, String minden_mezőnek_falsenak_kell_lennie) {
+        throw new UnsupportedOperationException("Not supported yet."); 
+    }
+
+    private void assertTrue(boolean ertek, String középső_mezőnek_truenak_kell_lennie) {
+        throw new UnsupportedOperationException("Not supported yet."); 
+    }
+
+    private void assertEquals(int i, int countTrue, String csak_1_mező_legyen_true_az_ujgomb_után) {
+        throw new UnsupportedOperationException("Not supported yet."); 
+    }
 }
